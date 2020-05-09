3b7c38e71b19743997b0956863327eb72af8b03d
Mehedi Hasan Emo <55323851+hasanemo@users.noreply.github.com>
2020-05-09 16:39:28 +0600

    Add files via upload

---
+++ Uva - Competitive Programming 3 - Super Easy/Uva_1124.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Uva_1124{
+  public static void main(String[] args)throws IOException{
+    BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+    BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
+    Scanner sc = new Scanner(System.in);
+    String s ;
+    while((s = br.readLine()) != null){
+      System.out.println(s);
+    }
+  }
+}
