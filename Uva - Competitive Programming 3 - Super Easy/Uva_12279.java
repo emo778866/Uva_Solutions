d60d8f2ce2f5f8bf269d0eb522fb2358d84163c9
Mehedi Hasan Emo <55323851+hasanemo@users.noreply.github.com>
2020-05-11 02:39:21 +0600

    Add files via upload

--- Uva - Competitive Programming 3 - Super Easy/Uva_12279.java
+++ Uva - Competitive Programming 3 - Super Easy/Uva_12279.java
@@ -5,26 +5,20 @@ public class Uva_12279{
     BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
     BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
     Scanner sc = new Scanner(System.in);
-    int o = 0;
-    int g = 0;
-    int i = 0;
-    int count = 1;
-    while(sc.hasNext()){
-      o = 0;
-      g = 0;
-      int c = sc.nextInt();
-      if(c!=0){
-        for(i=1;i<=c;i++){
-          int in = sc.nextInt();
-          if(in==0){
-            g++;
-          }
-          else{
-            o++;
-          }
-        }
-        System.out.println("Case "+(count++)+": "+(o-g));
+    int test = Integer.parseInt(br.readLine());
+    String s;
+    for(int t = 0; t < test; t++){
+      s = br.readLine();
+      if(s.length() == 5){
+        System.out.println("3");
       }
-    }
+      else if((s.contains("o") && s.contains("n")) ||(s.contains("o") && s.contains("e")) ||
+              (s.contains("n") && s.contains("e"))){
+        System.out.println("1");
+      }
+      else{
+        System.out.println("2");
+      }
+    } 
   }
-} 
+}
