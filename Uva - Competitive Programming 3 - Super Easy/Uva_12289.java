b3520b7154b14a54c0308a723e5520200c578d38
Mehedi Hasan Emo <55323851+hasanemo@users.noreply.github.com>
2020-05-11 02:48:21 +0600

    Update Uva_12289.java

--- Uva - Competitive Programming 3 - Super Easy/Uva_12289.java
+++ Uva - Competitive Programming 3 - Super Easy/Uva_12289.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-public class Uva_12279{
+public class Uva_12289{
   public static void main(String[] args)throws IOException{
     BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
     BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
