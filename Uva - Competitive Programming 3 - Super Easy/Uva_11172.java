e36711c6d1483b4547e92929cf26210982ebfbfa
Mehedi Hasan Emo <55323851+hasanemo@users.noreply.github.com>
2020-05-10 17:27:03 +0600

    Add files via upload

---
+++ Uva - Competitive Programming 3 - Super Easy/Uva_11172.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class Uva_11172{
+  public static void main(String[] args)throws IOException{
+    BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+    BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
+    Scanner sc = new Scanner(System.in);
+    int test = Integer.parseInt(br.readLine());
+    long n, m;
+    String array [];
+    for(int i = 0; i < test; i++){
+      array = br.readLine().split(" ");
+      n =  Long.parseLong(array[0]);
+      m =  Long.parseLong(array[1]); 
+      if(n > m){
+        System.out.println(">");
+      }
+      else if(n < m){
+        System.out.println("<");
+      }
+      else{
+        System.out.println("=");
+      }
+    }
+  }
+}
