794e17022598453fdeecbe5c81de7946f7f6af7b
Mehedi Hasan Emo <55323851+hasanemo@users.noreply.github.com>
2020-05-11 13:48:41 +0600

    Add files via upload

---
+++ Uva - Competitive Programming 3 - Super Easy/Uva_.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+class Uva_12250{
+  public static void main(String[] args)throws IOException{
+    BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+    BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
+    Scanner sc = new Scanner(System.in);
+    int o = 0;
+    int g = 0;
+    int i = 0;
+    int count = 1;
+    while(sc.hasNext()){
+      o = 0;
+      g = 0;
+      int c = sc.nextInt();
+      if(c!=0){
+        for(i=1;i<=c;i++){
+          int in = sc.nextInt();
+          if(in==0){
+            g++;
+          }
+          else{
+            o++;
+          }
+        }
+        System.out.println("Case "+(count++)+": "+(o-g));
+      }
+    }
+    bw.flush();
+  }
+}
