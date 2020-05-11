b75c5ebc3429efadbc42b3623fcb7c44dd33ec5c
Mehedi Hasan Emo <55323851+hasanemo@users.noreply.github.com>
2020-05-11 19:59:50 +0600

    Add files via upload

---
+++ Uva - Competitive Programming 3 - Easy/Uva_.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+class Uva_{
+  public static int f(int n){
+    String s = n+"";
+    int number = n;
+    while(s.length() != 1){
+      number = 0;
+      for(int i = 0; i < s.length(); i++){
+        number += Character.getNumericValue(s.charAt(i));
+      }
+      s = number+"";
+    }
+    return number;
+  }
+  public static void main(String[] args)throws IOException{
+    BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+    BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
+    Scanner sc = new Scanner(System.in);
+    int n;    
+    while((n = Integer.parseInt(br.readLine())) != 0){
+      System.out.println(f(n));
+    }
+    bw.flush();
+  }
+}
