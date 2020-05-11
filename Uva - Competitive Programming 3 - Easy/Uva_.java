5679fc7a0151f86e4b80650068f7fb556e3eb221
Mehedi Hasan Emo <55323851+hasanemo@users.noreply.github.com>
2020-05-11 23:57:53 +0600

    Add files via upload

---
+++ Uva - Competitive Programming 3 - Easy/Uva_.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+class Uva_{
+  public static void main(String[] args)throws IOException{
+    BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+    BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
+    Scanner sc = new Scanner(System.in);
+    int test = sc.nextInt();
+    int mile_rate, juice_rate, duration;
+    String m = "Mile";
+    String j = "Juice";
+    int n;
+    for(int t = 1; t <= test; t++){
+      mile_rate = 0;
+      juice_rate = 0;
+      System.out.print("Case "+t+": ");
+      n = sc.nextInt();
+      for(int i = 0; i < n; i++){
+        duration = sc.nextInt();
+        mile_rate += ((duration/30)+1)*10;
+        juice_rate += ((duration/60)+1)*15;
+      }
+      if(mile_rate < juice_rate){
+        System.out.println(m+" "+mile_rate);
+      }
+      else if(mile_rate > juice_rate){
+        System.out.println(j+" "+juice_rate);
+      }
+      else if(mile_rate == juice_rate){
+        System.out.println(m+" "+j+" "+mile_rate);
+      }
+    }
+    bw.flush();
+  }
+}
