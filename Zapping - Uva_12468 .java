e61949f85aed72f17f811f90b1a5608bb92ff351
Mehedi Hasan Emo <55323851+hasanemo@users.noreply.github.com>
2020-05-12 00:18:56 +0600

    Update and rename Uva - Competitive Programming 3 - Easy/Uva_.java to Zapping - Uva_12468 .java

--- Zapping - Uva_12468 .java
+++ Zapping - Uva_12468 .java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-class Uva_{
+public class Uva_12468 {
   public static void main(String[] args)throws IOException{
     BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
     BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
