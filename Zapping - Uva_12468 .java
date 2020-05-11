0b8a9f67fd434719283649d177807edf0b695dd5
Mehedi Hasan Emo <55323851+hasanemo@users.noreply.github.com>
2020-05-12 00:21:51 +0600

    Update Zapping - Uva_12468 .java

--- Zapping - Uva_12468 .java
+++ Zapping - Uva_12468 .java
@@ -24,3 +24,28 @@ public class Uva_12468 {
     bw.flush();
   }
 }
+//====Another Solution==========
+// import java.io.*;
+// import java.util.*;
+// class Uva_{
+//   public static void main(String[] args)throws IOException{
+//     BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+//     BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
+//     Scanner sc = new Scanner(System.in);
+//     int a, b, d;
+//     a = sc.nextInt();
+//     b =  sc.nextInt();
+//     while(a != -1 || b != -1){
+//       d = Math.abs(a-b);
+//       if(d > 50){
+//         System.out.println(100-d);
+//       }
+//       else{
+//         System.out.println(d);
+//       }
+//       a = sc.nextInt();
+//       b =  sc.nextInt();
+//     }
+//     bw.flush();
+//   }
+// }
