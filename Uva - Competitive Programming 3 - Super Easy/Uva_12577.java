8d00f8772e0861fb79dcd659abc05086e5d77b8d
Mehedi Hasan Emo <55323851+hasanemo@users.noreply.github.com>
2020-05-11 13:24:11 +0600

    Add files via upload

--- Uva - Competitive Programming 3 - Super Easy/Uva_12577.java
+++ Uva - Competitive Programming 3 - Super Easy/Uva_12577.java
@@ -1,19 +1,39 @@
 import java.io.*;
 import java.util.*;
-public class Uva_12577{
+class Uva_12498{
   public static void main(String[] args)throws IOException{
     BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
     BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
     Scanner sc = new Scanner(System.in);
     String line;
-    int c = 0;
-    while(!(line = br.readLine()).equals("*")){
-      bw.write("Case "+(++c)+": ");
-      if(line.equals("Hajj")){
-        bw.write("Hajj-e-Akbar"+"\n");
-      }
-      else{
-        bw.write("Hajj-e-Asghar"+"\n");
+    int q = 0;
+    int x, y, x_, y_;
+    String array [] = new String [2];
+    while(!(line = br.readLine()).equals("0")){
+      q = Integer.parseInt(line);
+      array = br.readLine().split(" ");
+      x = Integer.parseInt(array[0]);
+      y = Integer.parseInt(array[1]);
+      for(int i = 0; i < q; i++){
+        array = br.readLine().split(" ");
+        x_ = Integer.parseInt(array[0]);
+        y_ = Integer.parseInt(array[1]);
+        if(x_ == x || y_ == y){
+          bw.write("divisa");
+        }
+        else if(x_ > x && y_ > y){
+          bw.write("NE");
+        }
+        else if(x_ < x && y_ > y){
+          bw.write("NO");
+        }
+        else if(x_ < x && y_ < y){
+          bw.write("SO");
+        }
+        else{
+          bw.write("SE");
+        }
+        bw.write("\n");
       }
     }
     bw.flush();
