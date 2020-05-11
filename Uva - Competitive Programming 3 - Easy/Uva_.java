8cf198b3ac4c8476cde464ff7347131edd98ba57
Mehedi Hasan Emo <55323851+hasanemo@users.noreply.github.com>
2020-05-11 22:02:25 +0600

    Add files via upload

---
+++ Uva - Competitive Programming 3 - Easy/Uva_.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+class Uva_{
+  public static void main(String[] args)throws IOException{
+    BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+    BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
+    Scanner sc = new Scanner(System.in);
+    int test = Integer.parseInt(br.readLine());
+    int n, m;
+    String array [] = new String [10];
+    String s;
+    System.out.println("Lumberjacks:");
+    for(int t = 1; t <= test; t++){
+      s = "Ordered";
+      array = br.readLine().split(" ");
+      n = Integer.parseInt(array[0]);
+      m = Integer.parseInt(array[1]);
+      if(n > m){
+        for(int i = 2; i < 10; i++){
+          if(Integer.parseInt(array[i-1]) <= Integer.parseInt(array[i])){
+            s = "Unordered";
+            break;
+          }
+        }
+      }
+      else{
+        for(int i = 2; i < 10; i++){
+          if(Integer.parseInt(array[i-1]) >= Integer.parseInt(array[i])){
+            s = "Unordered";
+            break;
+          }
+        }
+      }
+      System.out.println(s);
+    }
+    bw.flush();
+  }
+}
