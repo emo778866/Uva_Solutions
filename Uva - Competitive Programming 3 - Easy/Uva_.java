69890051ba037e05517eac58d6d2876a29e03643
Mehedi Hasan Emo <55323851+hasanemo@users.noreply.github.com>
2020-05-12 13:32:27 +0600

    Add files via upload

---
+++ Uva - Competitive Programming 3 - Easy/Uva_.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+class Uva_{
+  public static void main(String[] args)throws IOException{
+    BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+    BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
+    Scanner sc = new Scanner(System.in);
+    String s;
+    String array [] = new String [3];
+    int n, count;
+    int test = Integer.parseInt(br.readLine());
+    for(int t = 0; t < test; t++){
+      count = 0;
+      n = Integer.parseInt(br.readLine());
+      int arr [] = new int [n+1];
+      for(int i = 1; i <= n; i++){
+        s = br.readLine();
+        if(s.equals("LEFT")){
+          arr[i] = -1;
+          count += arr[i];
+        }
+        else if(s.equals("RIGHT")){
+          arr[i] = 1;
+          count += arr[i];
+        }
+        else{
+          array = s.split(" ");
+          arr[i] =  arr[Integer.parseInt(array[2])];
+          count += arr[i];
+        }
+      }
+      System.out.println(count);
+    }
+    bw.flush();
+  }
+}
