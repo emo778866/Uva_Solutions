3422919dacefc7b71347068f261c3542f76e1f19
Mehedi Hasan Emo <55323851+hasanemo@users.noreply.github.com>
2020-05-12 15:06:27 +0600

    Update A Special "Happy Birthday" Song!!!Uva_12554.java

--- Uva - Competitive Programming 3 - Easy/A Special "Happy Birthday" Song!!!Uva_12554.java
+++ Uva - Competitive Programming 3 - Easy/A Special "Happy Birthday" Song!!!Uva_12554.java
@@ -13,15 +13,14 @@ public class Uva_12554{
     }; 
     int total_people = Integer.parseInt(br.readLine());
     String [] names = new String[total_people];  
-    for(int i = 0; i < total_people; i++){  
+    for(int i = 0; i < total_people; i++){
       names[i] = br.readLine();
-    }  
-    boolean isDone = false;
-    int i = 0;  
-    while(!isDone){  
-      System.out.println( names[i % total_people] + ": " + song[i%16] );  
+    }
+    int i = 0;
+    while(true){
+      System.out.println(names[(i)%total_people]+": "+song[i%16]);
       if(i%16 == 15 && i >= total_people){
-        isDone = true;  
+        break;
       }
       i++;
     }
